6531a001d72f6fa029476ea7f0dae57c901e6702
Nnlllllll <nathannovais020@gmail.com>
2025-05-05 09:42:14 -0300

    Update Gerente.java

--- Gerente.java
+++ Gerente.java
@@ -1,1 +1,13 @@
+public class Gerente extends Funcionario {
+    private double salarioBase;
+    private final double BONUS = 1000.0;
 
+    public Gerente(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    @Override
+    public double calcularSalarioFinal() {
+        return salarioBase + BONUS;
+    }
+}
